910976f500a66aa953d1f974dbcf1d2cd5b7f647
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2026-06-06 21:10:57 -0600

    protect route

--- routes/cars.js
+++ routes/cars.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-
 const carsController = require("../controllers/carsController");
+
+function isLoggedIn(req, res, next) {
+  if (req.user) {
+    return next();
+  }
+
+  return res.status(401).json({
+    message: "You must be logged in to access this route"
+  });
+}
 
 router.get("/", carsController.getAllCars);
 
 router.get("/:id", carsController.getSingleCar);
 
-router.post("/", carsController.createCar);
+router.post("/", isLoggedIn, carsController.createCar);
 
-router.put("/:id", carsController.updateCar);
+router.put("/:id", isLoggedIn, carsController.updateCar);
 
-router.delete("/:id", carsController.deleteCar);
+router.delete("/:id", isLoggedIn, carsController.deleteCar);
 
 module.exports = router;
